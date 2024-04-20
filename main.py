303d2796d615a8ed07d9a791697caf93c9f80d30
Gabriel Traiano <gabriel.traiano4@gmail.com>
2024-04-20 18:08:48 -0300

    im a little bit locked in this frame
    for while i dont know how i can
    select all the connect buttons
    i tried very presents classes, but ok i got this

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.keys import Keys
 from pathlib import Path
 import os
 from dotenv import load_dotenv
@@ -30,3 +31,19 @@ element_password = driver.find_element(By.XPATH, '//*[@id="password"]')
 element_password.send_keys(passw)
     
 driver.find_element(By.XPATH, '/html/body/div/main/div[3]/div[1]/div[3]/form/div[2]/button').click()
+
+search_el = driver.find_element(By.XPATH, '/html/body/div[5]/header/div/div/div/div[1]/input')
+search_el.send_keys('Developer', Keys.ENTER)
+
+morepeople = WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.XPATH,\
+    '/html/body/div[5]/div[3]/div[2]/div/div[1]/main/div/div/div[2]/div/div[2]/a')))
+
+morepeople.click()
+
+connectbuttons = driver.find_element(By.CLASS_NAME, 'artdeco-button__text')
+
+
+
+
+nextpage = driver.find_element(By.XPATH, '/html/body/div[5]/div[3]/div[2]/div/div[1]/main/div/div/div[5]/div/div/button[2]/span')
+
